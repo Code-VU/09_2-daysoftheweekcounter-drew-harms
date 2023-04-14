8deff430d4aefb623512747e06b45bc6156d3913
Drew Harms <Drew.Harms@vu.com>
2023-04-14 13:03:21 -0500

    count

--- counter.py
+++ counter.py
@@ -2,6 +2,24 @@ def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
 
+    day_count = {}
+    
+    with open(file_name) as file:
+        for line in file:
+            if line.startswith("From "):
+                day = grab_day(line)
+                day_count[day] = day_count.get(day, 0) + 1
+
+    print(day_count)
+
+
+def grab_day(line: str) -> str:
+    line = line.strip()
+    line_list = line.split()
+    day = line_list[2]
+
+    return day
+
 
 
 ## if you want to test locally run > python payCalculator.py
